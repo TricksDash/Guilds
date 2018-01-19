e2fbb0d1ad47e647bb4a3688c5c41894384225e9
Jamie <TricksDash@protonmail.com>
2018-01-19 08:46:02 +0000

    Update ChatFilterWords.java

--- src/com/elderrealm/main/utils/ChatFilterWords.java
+++ src/com/elderrealm/main/utils/ChatFilterWords.java
@@ -3,9 +3,6 @@ package com.elderrealm.main.utils;
 public class ChatFilterWords {
 	
 	public static String[] blockedWords = 
-		{"fuck", "bitch", "cunt", "nigga", "nigger", "bellend", 
-		"cock", "wanker", "fucker", "kys", "ddos", "asshole", "arsehole", "bastard", "dickhead", 
-		"dox", "dick", "lilfucker", "pussy", "fucked", "hoe", "slut", "slag", "bellend", "prick",
-		"n1gger", "n1gga", "twat", "fanny", "anal", "sex", "paki", "terrorist", "911", "bomb", "suicide", "fanny"};
+		{"cuss1", "cuss2"};
 	
 }
